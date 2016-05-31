b14510bdf848e44dd92010fa3b55bb297df04a60
Rémy HUBSCHER <rhubscher@mozilla.com>
2016-05-31 14:57:28 +0200

    Display the URL the file is at.

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -15,3 +15,6 @@ except:
 
 print('Uploading %s to Amazon S3 bucket %s' % (FILENAME, BUCKET_NAME))
 s3.Object(BUCKET_NAME, 'lambda.zip').put(Body=open(FILENAME, 'rb'))
+
+print('File uploaded to https://s3.%s.amazonaws.com/%s/lambda.zip' %
+      (AWS_REGION, BUCKET_NAME))
