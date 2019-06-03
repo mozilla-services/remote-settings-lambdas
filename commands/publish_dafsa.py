482e8641c35f0df46b6128484f79ff60233e6c82
Arpit Bharti <rpittrer@gmail.com>
2019-06-03 23:16:48 +0530

    Replaced wget dependency and added kinto record publishing code

--- commands/publish_dafsa.py
+++ commands/publish_dafsa.py
@@ -1,17 +1,21 @@
 import requests
 from requests.exceptions import HTTPError
-import wget
 import tempfile
 import subprocess
+from kinto_http import Client
 
 COMMIT_HASH_URL = "https://api.github.com/repos/publicsuffix/list/commits/master"
+
 LIST_URL = (
     "https://raw.githubusercontent.com/publicsuffix/list/master/public_suffix_list.dat"
 )
+
 MAKE_DAFSA_PY = "https://raw.githubusercontent.com/arpit73/temp_dafsa_testing_repo/master/publishing/make_dafsa.py"
 PREPARE_TLDS_PY = "https://raw.githubusercontent.com/arpit73/temp_dafsa_testing_repo/master/publishing/prepare_tlds.py"
 
-DAFSA_OUTPUT = "./etld_data.inc"
+
+SERVER = "https://kinto.dev.mozaws.net/v1"
+CREDENTIALS = ("arpit73", "s3cr3t")  # (username, password)
 
 
 def get_latest_hash(url):
@@ -23,29 +27,57 @@ def get_latest_hash(url):
     except Exception as err:
         print(f"Other error occurred: {err}")
     else:
-        print("Failed")
+        print("Fetching Failed")
 
 
 # can be replaced with requests later to avoid extra dependency
 def download_resources(*urls, **kwargs):
     for url in urls:
-        wget.download(url, out=kwargs["directory"])
+        file_location = kwargs["directory"] + "/" + url.split("/")[-1]
+        print(file_location)
+        try:
+            response = requests.get(url, stream=True)
+            with open(file_location, "wb") as f:
+                for chunk in response.iter_content(chunk_size=1024):
+                    if chunk:
+                        f.write(chunk)
+                    else:
+                        print("Error!!!")
+        except HTTPError as http_err:
+            print(f"HTTP error occurred: {http_err}")
+        except Exception as err:
+            print(f"Other error occurred: {err}")
 
 
 # TODO
 def check_last_stored_hash():
-    pass
+    return True
 
 
-latest_hash = get_latest_hash(COMMIT_HASH_URL)
+def publish_dafsa(server, credentials, commit_hash):
+
+    bucket_id = "firefox-core-network-dns"
+    collection_id = "public-suffix-list"
+    record_id = "latest-commit-hash"
+
+    client = Client(server_url=SERVER, auth=credentials)
+    client.create_bucket(id=bucket_id)
+    client.create_collection(id=collection_id, bucket=bucket_id)
+
+    client.create_record(
+        id=record_id,
+        data={"latest-commit-hash": commit_hash},
+        collection=collection_id,
+        bucket=bucket_id,
+    )
 
 
 with tempfile.TemporaryDirectory() as tmp:
     # print(tmp)
     download_resources(LIST_URL, MAKE_DAFSA_PY, PREPARE_TLDS_PY, directory=tmp)
     """
-       prepare_tlds.py is called with the two arguments the location 
-       of the downloaded psl and the name of the output file
+       prepare_tlds.py is called with the two arguments the location
+       of the downloaded public suffix list and the name of the output file
     """
     subprocess.run(
         [
@@ -55,10 +87,7 @@ with tempfile.TemporaryDirectory() as tmp:
             f"{tmp}/etld_data.inc",
         ]
     )
-    # import prepare_tlds
-    # prepare_tlds.main(DAFSA_OUTPUT, PSL_LOCATION)
-    path = tmp + "/etld_data.inc"
-    with open(path, "r") as f:
-        print(f.read())
-    # import make_dafsa
-    # import prepare_tlds
+    subprocess.run(["ls",tmp])
+    latest_hash = get_latest_hash(COMMIT_HASH_URL)
+    # publish_dafsa(SERVER, CREDENTIALS, latest_hash)
+
