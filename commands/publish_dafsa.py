de83ba223bb21dd9b6416739b30a3c37abdebbe0
Arpit Bharti <rpittrer@gmail.com>
2019-06-05 14:47:12 +0530

    script refactored, publish_dafsa now entry point for script

--- commands/publish_dafsa.py
+++ commands/publish_dafsa.py
@@ -18,6 +18,10 @@ PREPARE_TLDS_PY = "https://raw.githubusercontent.com/arpit73/temp_dafsa_testing_
 SERVER = "https://kinto.dev.mozaws.net/v1"
 CREDENTIALS = ("arpit73", "s3cr3t")  # (username, password)
 
+BUCKET_ID = "firefox-core-network-dns"
+COLLECTION_ID = "public-suffix-list"
+RECORD_ID = "latest-commit-hash"
+
 
 def handle_request_errors(func):
     def inner(*args, **kwargs):
@@ -54,61 +58,43 @@ def download_resources(*urls, **kwargs):
                     print("Error!!!")
 
 
-# TODO
-def check_last_stored_hash():
-    return True
+def record_exists(record):
+    # will check if a valid record exists already a and return true if so
+    pass
 
 
-def publish_dafsa(server, credentials, commit_hash):
-
-    bucket_id = "firefox-core-network-dns"
-    collection_id = "public-suffix-list"
-    record_id = "latest-commit-hash"
-
-    client = Client(server_url=server, auth=credentials)
-
-    latest_hash = get_latest_hash(COMMIT_HASH_URL)
-
-    try:
-        record = client.get_record(
-            id=record_id, bucket=bucket_id, collection=collection_id
+def make_dafsa_and_publish(client, latest_hash):
+    with tempfile.TemporaryDirectory() as tmp:
+        download_resources(LIST_URL, MAKE_DAFSA_PY, PREPARE_TLDS_PY, directory=tmp)
+        """
+        prepare_tlds.py is called with the two arguments the location of 
+        the downloaded public suffix list and the name of the output file
+        """
+        subprocess.run(
+            [
+                "python3",
+                f"{tmp}/prepare_tlds.py",
+                f"{tmp}/public_suffix_list.dat",
+                f"{tmp}/etld_data.inc",
+            ]
         )
-    except KintoException as e:
-        print(e)
-        client.create_record(
-            id=record_id,
-            data={"latest-commit-hash": commit_hash},
-            collection=collection_id,
-            bucket=bucket_id,
-        )
-
-
-    if record["data"]["latest-commit-hash"] == latest_hash:
-        sys.exit()
-    else:
+        subprocess.run(["ls", tmp])
         client.update_record(
-            id=record_id,
-            data={"latest-commit-hash": commit_hash, "blah": "blah"},
-            collection=collection_id,
-            bucket=bucket_id,
+            id=RECORD_ID,
+            data={"latest-commit-hash": latest_hash},
+            collection=COLLECTION_ID,
+            bucket=BUCKET_ID,
         )
 
 
-with tempfile.TemporaryDirectory() as tmp:
-    # print(tmp)
-    download_resources(LIST_URL, MAKE_DAFSA_PY, PREPARE_TLDS_PY, directory=tmp)
-    """
-       prepare_tlds.py is called with the two arguments the location of 
-       the downloaded public suffix list and the name of the output file
-    """
-    subprocess.run(
-        [
-            "python3",
-            f"{tmp}/prepare_tlds.py",
-            f"{tmp}/public_suffix_list.dat",
-            f"{tmp}/etld_data.inc",
-        ]
-    )
-    subprocess.run(["ls", tmp])
+def publish_dafsa():
+    client = Client(server_url=SERVER, auth=CREDENTIALS)
     latest_hash = get_latest_hash(COMMIT_HASH_URL)
-    publish_dafsa(SERVER, CREDENTIALS, latest_hash)
+    record = client.get_record(id=RECORD_ID, bucket=BUCKET_ID, collection=COLLECTION_ID)
+    if record_exists(record):
+        if record["data"]["latest-commit-hash"] == latest_hash:
+            return 0
+        else:
+            make_dafsa_and_publish(client, latest_hash)
+    else:
+        make_dafsa_and_publish(client, latest_hash)
