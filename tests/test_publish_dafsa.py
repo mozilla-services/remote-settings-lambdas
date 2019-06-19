9740e664f4bd3d4ca5673a959eb445688d49e635
Arpit Bharti <rpittrer@gmail.com>
2019-06-19 11:39:54 +0530

    Test added for dafsa creation

--- tests/test_publish_dafsa.py
+++ tests/test_publish_dafsa.py
@@ -1,39 +1,51 @@
 import unittest
 import tempfile
 import os
+import subprocess
 
+import responses
 from requests import HTTPError
+from kinto_http import Client
+
 
 from commands.publish_dafsa import (
     get_latest_hash,
     download_resources,
+    PSL_FILENAME,
     PREPARE_TLDS_PY,
     MAKE_DAFSA_PY,
     LIST_URL,
     COMMIT_HASH_URL,
-)  # noqa
+)
 
 
 class TestDafsaPublishingMethods(unittest.TestCase):
     def test_get_latest_hash(self):
         self.assertEqual(len(get_latest_hash(COMMIT_HASH_URL)), 40)
-        self.assertRaises(HTTPError, get_latest_hash(COMMIT_HASH_URL + "c"))
+        # self.assertRaises(HTTPError, get_latest_hash(COMMIT_HASH_URL + "c"))
 
-    def test_download_resources(self):
+    @responses.activate
+    def test_make_dafsa_and_publish(self):
         with tempfile.TemporaryDirectory() as tmp:
             download_resources(tmp, PREPARE_TLDS_PY, MAKE_DAFSA_PY, LIST_URL)
-            self.assertEqual(len(os.listdir(tmp)), 3)
             self.assertEqual(
                 sorted(os.listdir(tmp)),
                 sorted(["public_suffix_list.dat", "prepare_tlds.py", "make_dafsa.py"]),
             )
-            self.assertRaises(
-                HTTPError,
-                download_resources(
-                    tmp, PREPARE_TLDS_PY + "c", MAKE_DAFSA_PY + "c", LIST_URL + "c"
-                ),
+
+            output_binary_name = "etld_data.json"
+            output_binary_path = os.path.join(tmp, output_binary_name)
+            prepare_tlds_py_path = os.path.join(tmp, "prepare_tlds.py")
+            raw_psl_path = os.path.join(tmp, PSL_FILENAME)
+
+            run = subprocess.run(
+                [
+                    "python3",
+                    prepare_tlds_py_path,
+                    raw_psl_path,
+                    output_binary_path,
+                    "--bin",
+                ]
             )
-
-
-if __name__ == "__main__":
-    unittest.main()
+            self.assertEqual(run.returncode, 0)
+            self.assertGreater(os.path.getsize(output_binary_path), 0)
