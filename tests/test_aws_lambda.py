6d00d3216539443f1d370dd6f1a6f02f5d87f2f5
Mathieu Leplatre <mathieu@mozilla.com>
2019-07-03 18:33:34 +0200

    Fix error with __path__

--- tests/test_aws_lambda.py
+++ tests/test_aws_lambda.py
@@ -1,8 +1,8 @@
-from aws_lambda import main
+import aws_lambda
 
 
 def test_help(capsys):
-    return_code = main("help")
+    return_code = aws_lambda.main("help")
     assert not return_code
     captured = capsys.readouterr()
     assert "Available commands:" in captured.out
@@ -10,7 +10,7 @@ def test_help(capsys):
 
 
 def test_unknown_command(capsys):
-    return_code = main("neverheardof")
+    return_code = aws_lambda.main("neverheardof")
     assert return_code
     captured = capsys.readouterr()
     assert "Available commands:" in captured.out
